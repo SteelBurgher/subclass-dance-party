59cd3dfc6fa69cc41dff5cad18da513cc23448d0
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-30 23:10:11 -0400

    Created a new Beyonce dance type. Started making the code for the "line up" button.

--- src/SquareDancer.js
+++ src/SquareDancer.js
@@ -1,7 +1,7 @@
 var MakeSquareDancer = function(top, left, timeBetweenSteps){
   MakeDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('square');
-
+  this.pulse();
 
 };
 
@@ -9,8 +9,12 @@ MakeSquareDancer.prototype = Object.create(MakeDancer.prototype);
 MakeSquareDancer.prototype.constructor = MakeSquareDancer;
 MakeSquareDancer.prototype.oldStep = MakeDancer.prototype.step;
 
+MakeSquareDancer.prototype.pulse = function() {
+  setTimeout(this.pulse.bind(this), 1000);
+  if (this.$node.hasClass('expand')) {
+    this.$node.removeClass('expand');
+  } else{
+    this.$node.addClass('expand');
+  }
 
-MakeSquareDancer.prototype.step = function(){
-    this.oldStep();
-    this.$node.toggle();
-  };
+};
